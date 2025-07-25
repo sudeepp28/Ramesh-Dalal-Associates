4bff57b7a7c90f19e8c1cab6221eb1beface41b1
sudeepp28 <122432690+sudeepp28@users.noreply.github.com>
2025-07-25 17:53:14 +0530

    cdf

--- src/app/contact/cfirst/cfirst.component.ts
+++ src/app/contact/cfirst/cfirst.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cfirst',
@@ -6,6 +6,21 @@ import { Component } from '@angular/core';
   templateUrl: './cfirst.component.html',
   styleUrl: './cfirst.component.css'
 })
-export class CfirstComponent {
- email = 'ramesh.labourlawconsultant@gmail.com';
+export class CfirstComponent  implements OnInit {
+  email = 'ramesh.labourlawconsultant@gmail.com';
+  isMobile = false;
+  copied = false;
+
+  ngOnInit(): void {
+    // Simple device check using navigator.userAgent
+    const ua = navigator.userAgent.toLowerCase();
+    this.isMobile = /iphone|ipad|ipod|android/.test(ua);
+  }
+
+  copyEmail(): void {
+    navigator.clipboard.writeText(this.email).then(() => {
+      this.copied = true;
+      setTimeout(() => this.copied = false, 2000);
+    });
+  }
 }
